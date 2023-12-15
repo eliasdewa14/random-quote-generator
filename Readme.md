c6afa778776ce81ac9701bf793f044bb011bad8e
Elias-Dewa <elias.dewa19@gmail.com>
2023-12-15 04:22:55 -0800

    Project: initial commit

---
+++ Readme.md
@@ -0,0 +1,31 @@
+# Random Quote Generator
+
+![Alt text](image-1.png)
+
+## Description
+
+This is a simple random quote generator project with some nice functionality and features, like the ability to copy and tweet out a quote on Twitter. It's also a mobile responsive.
+
+## Demo
+
+You can try it out [here](https://eliasdewa14.github.io/hangman-js/)
+
+## How was it built?
+
+* This was built using HTML, CSS, and vanilla JavaScript.
+* A [Random Quote API](http://api.quotable.io/random) and a [Web Search API](https://developer.mozilla.org/en-US/docs/Web/API/SpeechSynthesisUtterance) were used
+
+## How to use
+
+* Clone from Github
+* Make any changes that you like
+* Open `index.html` and try it out
+
+## Contributing
+
+Pull requests are welcome. For major changes, please open an issue first
+to discuss what you would like to change.
+
+## License
+
+[MIT](https://choosealicense.com/licenses/mit/)
---
+++ script.js
@@ -0,0 +1,56 @@
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const copyButton= document.getElementById('copy');
+const soundButton= document.getElementById('sound');
+const twitterButton= document.getElementById('twitter');
+const newQuoteButton = document.getElementById('new-quote');
+const loader = document.getElementById('loader');
+
+// Get random quote from API
+async function getQuote() {
+  newQuoteButton.classList.add('loading');
+  newQuoteButton.innerText = "Loading Quote...";
+  const apiUrl = 'http://api.quotable.io/random';
+  try {
+    const response = await fetch(apiUrl);
+    const data = await response.json();
+
+    // Reduce font size for long quote
+    (data.content.length > 100) ? quoteText.classList.add('long-quote') : quoteText.classList.remove('long-quote');
+
+    quoteText.innerText = data.content;
+
+    // If the Author is blank, add 'Unknown'
+    (data.author === '') ? authorText.innerText = 'Unknown' : authorText.innerText = data.author;
+
+    newQuoteButton.innerText = "New Quote";
+    newQuoteButton.classList.remove('loading');
+  } catch (error) {
+    console.log('Sorry, no quote was found', error);
+  }
+}
+
+// Event Listener
+soundButton.addEventListener('click', () => {
+  // The SpeechSynthesisUtterance is a web speech api that represents a speech requests
+  let utterance = new SpeechSynthesisUtterance(`${quoteText.innerText} by ${authorText.innerText}`);
+  speechSynthesis.speak(utterance);
+});
+
+copyButton.addEventListener('click', () => {
+  // Copy the quote text
+  navigator.clipboard.writeText(quoteText.innerText);
+});
+
+twitterButton.addEventListener('click', () => {
+  const quote = quoteText.innerText
+  const author = authorText.innerText
+  const twitterUrl = `https://twitter.com/intent/tweet?text=${quote} - ${author}`;
+  window.open(twitterUrl, '_blank');
+});
+
+newQuoteButton.addEventListener('click', getQuote);
+
+// On load
+getQuote();
